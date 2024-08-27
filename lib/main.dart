5e65f7e198e4bfa6c03721e9987ce066efbc207f
Shihab Kandil <shihab.mostafa1@gmail.com>
2024-08-28 00:21:16 +0300

    feat: Add flutter localizations

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
 import 'common/constants/theme/app_theme_data.dart';
 import 'common/core/injector/locator.dart';
 import 'common/core/router/app_router.dart';
+import 'l10n/generated/app_localizations.dart';
 
 void commonMain() {
   WidgetsFlutterBinding.ensureInitialized();
@@ -23,6 +25,12 @@ class App extends StatelessWidget {
     return MaterialApp.router(
       theme: AppThemeData.lightTheme,
       routerConfig: appRouter.config(),
+      supportedLocales: const [Locale('en')],
+      localizationsDelegates: const [
+        AppLocalizations.delegate,
+        GlobalCupertinoLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+      ],
     );
   }
 }
