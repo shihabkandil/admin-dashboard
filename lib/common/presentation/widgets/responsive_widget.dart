7989d98643844e5351b4f1debd7323042ba25342
Shihab Kandil <shihab.mostafa1@gmail.com>
2024-09-30 17:09:47 +0300

    refactor: Rename responsive_builder.dart to responsive_widget.dart for clarity

--- lib/common/presentation/widgets/responsive_widget.dart
+++ lib/common/presentation/widgets/responsive_widget.dart
@@ -1,23 +1,24 @@
+import 'package:admin_dashboard/common/constants/constants.dart';
 import 'package:flutter/material.dart';
 
 class ResponsiveWidget extends StatelessWidget {
-  final Widget wideScreen;
-  final Widget mobileScreen;
+  final Widget wide;
+  final Widget mobile;
 
   const ResponsiveWidget({
     super.key,
-    required this.wideScreen,
-    required this.mobileScreen,
+    required this.wide,
+    required this.mobile,
   });
 
   @override
   Widget build(BuildContext context) {
     return LayoutBuilder(
       builder: (context, constraints) {
-        if (constraints.maxWidth > 800) {
-          return wideScreen;
+        if (constraints.maxWidth > Constants.wideScreenBreakpointPixels) {
+          return wide;
         } else {
-          return mobileScreen;
+          return mobile;
         }
       },
     );
