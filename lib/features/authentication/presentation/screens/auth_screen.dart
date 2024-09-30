817aecef68091b3baa9ffb56844698889debf205
Shihab Kandil <shihab.mostafa1@gmail.com>
2024-09-30 17:10:50 +0300

    fix: auth_screen.dart imports

--- lib/features/authentication/presentation/screens/auth_screen.dart
+++ lib/features/authentication/presentation/screens/auth_screen.dart
@@ -2,9 +2,9 @@ import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
-import '../cubits/auth_switcher_cubit/auth_switcher_cubit.dart';
 import '../../../../common/constants/theme/app_colors.dart';
-import '../../../../common/presentation/widgets/responsive_builder.dart';
+import '../../../../common/presentation/widgets/responsive_widget.dart';
+import '../cubits/auth_switcher_cubit/auth_switcher_cubit.dart';
 import '../widgets/auth_mobile_body.dart';
 import '../widgets/auth_wide_body.dart';
 
@@ -26,8 +26,8 @@ class AuthScreen extends StatelessWidget {
         child: BlocProvider(
           create: (context) => AuthSwitcherCubit(),
           child: const ResponsiveWidget(
-            wideScreen: AuthWideBody(),
-            mobileScreen: AuthMobileBody(),
+            wide: AuthWideBody(),
+            mobile: AuthMobileBody(),
           ),
         ),
       ),
